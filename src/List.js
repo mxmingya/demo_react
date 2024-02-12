2f6436e6fa8f6a7ebce79587eabf4f9d76a33e45
Mingya Ma <brucelee@MacBook-Air.local>
2024-02-11 20:40:48 -0500

    the compoennts

---
+++ src/List.js
@@ -0,0 +1,34 @@
+const customers = [
+    {
+        name: "Jeremy Lam",
+        age: 27,
+        location: "Seattle",
+        income: "$10000"
+    },
+    {
+        name: "Mike Chen",
+        age: 42,
+        location: "New York",
+        income: "$10000"
+    },
+    {
+        name: "Brian Smith",
+        age: 21,
+        location: "Minneapolis",
+        income: "$12000"
+    },
+    {
+        name: "John Doe",
+        age: 52,
+        location: "Tokyo",
+        income: "$100003"
+    },
+    {
+        name: "Marry Jane",
+        age: 33,
+        location: "Alabama",
+        income: "$120000"
+    },
+]
+
+export default customers;
